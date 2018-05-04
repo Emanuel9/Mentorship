1ac74881422a82aaaadef1796652f1d37edd940f
Marius.Herta <Marius.Herta@EB-OR6135367.office.orange.intra>
2018-05-04 14:42:55 +0300

    Initial commit - added register rest service
    Added register page.
    
    Added UserRole enum.
    
    Partial register service functionality.
    
    Added password hash functionality.
    
    Added password hash functionality and minor fixes.
    
    Added Spring security.
    
    Added JS password match validation.
    
    Fixed javascript code.
    
    Completed pom.xml.
    
    Updated pom.xml

---
+++ otheatre/src/main/resources/static/js/register.js
@@ -0,0 +1,21 @@
+console.log("Gaseste fisierul")
+//
+/////* 
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//
+//window.alert(123)
+const registerButton = $("#register_button");
+const password = $("#password");
+const password_confirmation = $("#password_confirmation");
+
+registerButton.click(function(event){
+    event.preventDefault();
+    if(password !== password_confirmation){
+        window.alert("Passwords do not match!");
+    }
+
+});
+
