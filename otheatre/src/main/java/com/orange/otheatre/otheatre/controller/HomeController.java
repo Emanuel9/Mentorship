8deff4cf5f4ddefb1629a5ebe01188310c184a66
Marius Herta <marius.herta@gmail.com>
2018-06-03 20:49:23 +0300

    Added Logger.
    Fixed the anonymous authentication problem
    
    Final logger
    
    Corected notes
    
    Added logs folder to .gitignore
    
    Added logs folder to .gitignore
    
    Deleted logs folder

--- otheatre/src/main/java/com/orange/otheatre/otheatre/controller/HomeController.java
+++ otheatre/src/main/java/com/orange/otheatre/otheatre/controller/HomeController.java
@@ -2,6 +2,9 @@ package com.orange.otheatre.otheatre.controller;
 
 
 import com.orange.otheatre.otheatre.entities.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,21 +18,28 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class HomeController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping(method = RequestMethod.GET, value = "/")
     public String homePage(HttpSession session, Model model) {
         SecurityContext securityContext = SecurityContextHolder.getContext();
         Authentication authentication = securityContext.getAuthentication();
 
-        if ( authentication != null ) {
-
+        if ( SecurityContextHolder.getContext().getAuthentication() != null &&
+                SecurityContextHolder.getContext().getAuthentication().isAuthenticated() &&
+                //when Anonymous Authentication is enabled
+                !(SecurityContextHolder.getContext().getAuthentication()
+                        instanceof AnonymousAuthenticationToken) ) {
+            LOGGER.info("Homepage: Checking if user is authenticated.");
             try {
                 User principal = (User) authentication.getPrincipal();
                 model.addAttribute("principal", principal);
+                LOGGER.info("Homepage: Found authenticated user {}", principal.getEmail());
             } catch ( Exception ex ) {
-
+                LOGGER.debug("Homepage: " + ex.toString() + String.valueOf(ex.getStackTrace()));
             }
         }
-
+        LOGGER.info("Homepage: Displaying homePage.");
         return "homePage";
     }
 }
