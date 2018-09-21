490aceaf4c955d2fbf87cae9ba395556fa240d10
Marius Herta <marius.herta@gmail.com>
2018-09-21 17:41:22 +0300

    test

--- otheatre/src/main/resources/db/migration/V2018.09.21.17.31__insert_user_profile_for_admin.sql
+++ otheatre/src/main/resources/db/migration/V2018.09.21.17.31__insert_user_profile_for_admin.sql
@@ -1,2 +1,5 @@
+ALTER TABLE `user_profile`
+  MODIFY `user_profile_id` int(11) NOT NULL AUTO_INCREMENT
+
 INSERT INTO `user_profile` (`bio`, `birthday`, `email`, `first_name`, `last_name`, `link_to_profile_picture`, `user_user_id`) VALUES
 ("Sunt un boss bre", '1994-05-21', 'admin@otheatre.com', 'admin','adminescu', NULL, 1);
