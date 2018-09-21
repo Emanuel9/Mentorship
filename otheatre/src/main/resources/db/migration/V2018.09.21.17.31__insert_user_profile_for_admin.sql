d9db9ff789fb27049bdc99cd5ef189f5a251cdd0
Marius Herta <marius.herta@gmail.com>
2018-09-21 17:36:06 +0300

    test

---
+++ otheatre/src/main/resources/db/migration/V2018.09.21.17.31__insert_user_profile_for_admin.sql
@@ -0,0 +1,2 @@
+INSERT INTO `user_profile` (`bio`, `birthday`, `email`, `first_name`, `last_name`, `link_to_profile_picture`, `user_user_id`) VALUES
+("Sunt un boss bre", '1994-05-21', 'admin@otheatre.com', 'admin','adminescu', NULL, 1);
